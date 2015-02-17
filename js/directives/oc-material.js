dd24dacab8dd53a468680672b3dca769b6fbe489
Orion <orioncharlier@gmail.com>
2015-02-18 00:33:43 +0100

    Tests

--- js/directives/oc-material.js
+++ js/directives/oc-material.js
@@ -22,15 +22,28 @@ angular.module('starter.controllers')
 						var first = elements_[index].getBoundingClientRect();
 
 						// Now set the element to the last position.
-						elements_[index].classList.toggle('item--expanded');
+						elements_[index].classList.add('item--expanded');
 
 						// Read again. This forces a sync layout, so be careful.
 						var last = elements_[index].getBoundingClientRect();
 
 						// Invert.
-						var invert = first.top - last.top;
-						//elements_[index].style.transform = 'translateY(' + invert + 'px)';
+						var invertHeight = first.top - last.top;
+						var invertWidth = first.left - last.left;
+						var invertScale = first.height / last.height;
+						
+						elements_[index].style.transform = 'translateY(' + invertHeight + 'px) translateX(' + invertWidth + 'px)';
+						if(index == 'image') {
+							elements_[index].style.transform += 'scale(' + invertScale + ')';
+						}
 					};
+				}
+				else {
+					expanded_ = false;
+					for(var index in elements_) {
+						elements_[index].classList.remove('item--expanded');
+						elements_[index].style.transform = '';
+					}
 				}
 			});
 
